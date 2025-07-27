8528948fb95843b0ef5a82663fc6292a2587abe5
Millimeter <akumakumu@gmail.com>
2025-07-27 07:53:52 +0700

    base init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, routing::get};
+
+async fn root() -> &'static str {
+    "Hi Wakimae"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
