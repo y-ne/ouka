08c16d4381f402e3e661eb2cf5ce13b9fa6a1106
Millimeter <akumakumu@gmail.com>
2025-07-27 07:57:36 +0700

    seperate handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use axum::{Router, routing::get};
 
-async fn root() -> &'static str {
-    "Hi Wakimae"
-}
+mod handlers;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(handlers::root));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
